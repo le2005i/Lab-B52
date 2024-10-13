8b4e23b1ae02c88c67fa3e7f34acf46e4e9e8056
le2005i <159822642+le2005i@users.noreply.github.com>
2024-10-13 09:25:53 +0300

    mo

--- Lab Practice/Lab 6 – Lists and State Management/stadium_app/lib/home_page.dart
+++ Lab Practice/Lab 6 – Lists and State Management/stadium_app/lib/home_page.dart
@@ -10,6 +10,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stadium App'),
+        actions: [
+          IconButton(onPressed: (){}, icon: const Icon(Icons.search))
+        ],
+      ),
+      body: const Column(
+        children: [
+          
+          Text("hello"), TextField(decoration: InputDecoration(border: OutlineInputBorder()),)],
+      ),
+    );
   }
 }
